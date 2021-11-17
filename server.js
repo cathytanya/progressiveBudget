614ed81ef56695ab88288c8fbe90f0406b694529
cathytanya <cathycolette.tanya@gmail.com>
2021-11-17 00:53:21 -0500

    heroku push

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// Malware
+// Middleware
 app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
