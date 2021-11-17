21c060f3a74725463c442eff40b22b1175be9f5d
cathytanya <cathycolette.tanya@gmail.com>
2021-11-16 20:11:28 -0500

    removed lines from the code which was suppose to be in the server.js

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,8 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
-// adding the dependencies(express and mongoose)
-const express = require("express");
-const mongoose = require("mongoose")
 
 const app = express();
-// HEROKU PORT
-const PORT = process.env.PORT || 3000;
+
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
