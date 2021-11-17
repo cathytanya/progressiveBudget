cb1c948cb7e47f42909d4a092ef2f9252b540f03
cathytanya <cathycolette.tanya@gmail.com>
2021-11-16 21:06:27 -0500

    cache variables made and comment

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,6 @@ const FILES_TO_CACHE = [
     "/assets/images/icons/icon-192x192.png",
     "/assets/images/icons/icon-512x512.png"
   ];
+
+  const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
