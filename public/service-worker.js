d11ff2eb90f53f72eaf38f122d8082d7dd04f2d6
cathytanya <cathycolette.tanya@gmail.com>
2021-11-16 20:48:28 -0500

    change the path in ther service worker

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/assets/css/styles.css",
+    "/assets/js/index.js",
+    "/manifest.webmanifest",
+    // "/favicon.ico",
+    "/assets/images/icons/icon-72x72.png",
+    "/assets/images/icons/icon-96x96.png"
+  ];
